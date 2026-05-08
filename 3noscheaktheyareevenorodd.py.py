8c20ef8fe5bd115cec10085db883f72e334e22dd
yashkadam917-art <yashkadam917@gmail.com>
2026-05-08 23:19:48 +0530

    Add files via upload

---
+++ 3noscheaktheyareevenorodd.py.py
@@ -0,0 +1,18 @@
+a = int(input("Enter num 1: "))
+b = int(input("Enter num 2: "))
+c = int(input("Enter num 3: "))
+
+if a % 2 == 0:
+    print(a, "is even")
+else:
+    print(a, "is odd")
+
+if b % 2 == 0:
+    print(b, "is even")
+else:
+    print(b, "is odd")
+
+if c % 2 == 0:
+    print(c, "is even")
+else:
+    print(c, "is odd")
